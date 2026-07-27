34afefb44c75f09ea2edd44588e9a01f6f03882a
angienx <angie.nacato3a@gmail.com>
2026-07-26 20:00:43 -0500

    Update

--- 06Code/MothersApp_1/MothersApp/build/classes/ec/edu/espe/mothersApp/model/Interfaces.rs
+++
@@ -1,5 +0,0 @@
-ec.edu.espe.mothersApp.model.IValidator
-ec.edu.espe.mothersApp.model.ISearchable
-ec.edu.espe.mothersApp.model.IAppointment
-ec.edu.espe.mothersApp.model.IProfile
-ec.edu.espe.mothersApp.model.IRecordRepository
